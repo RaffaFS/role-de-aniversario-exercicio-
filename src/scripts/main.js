6faa324f2ad09ec82b85b964b02012bac40f0f6e
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-05-26 12:06:06 -0300

    alterando texto da alimentação

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 AOS.init();
 
 const dataAniversario = new Date("Jan 31, 2025 10:00:00");
-const timeStampAniversario = dataAniversario.getTime();
+const timeStampAniversario = dataAniversario.getTime();     // Essa função pega o valor em "Ms" referente à data definida em "dataAaniversário"
+                                                            // Dizemos então que esse valor é o "timeStamp" de tal instante, sendo que
+                                                            // O "instante zero" seria o dia 01/01/1970 às 00:00 em UTC ou
+                                                            // "Universal Time Coordinated" / "Tempo Universal Cordenado"
 
 const contaTempo = setInterval(function(){
     const agora = new Date();
@@ -15,9 +18,10 @@ const contaTempo = setInterval(function(){
     const segundosEmMs = 1000;
 
     const diasRestantes = Math.floor(timeStampRestante / diasEmMs);
-    const horasRestantes = Math.floor((timeStampRestante % diasEmMs) / horasEmMs);
-    const minutosRestantes = Math.floor((timeStampRestante % horasEmMs) / minutosEmMs);
-    const segundosRestantes = Math.floor((timeStampRestante % minutosEmMs) / segundosEmMs);
+    const horasRestantes = Math.floor((timeStampRestante % diasEmMs) / horasEmMs);      // O operador "%" é um operador de divisão, mas trará
+    const minutosRestantes = Math.floor((timeStampRestante % horasEmMs) / minutosEmMs);     // para mim, como resultado, o "resto" da divisão
+    const segundosRestantes = Math.floor((timeStampRestante % minutosEmMs) / segundosEmMs); // que pode ser caracterizado, nesse caso, pelo
+                                                                                            // tempo corrido de um dia não completo (em "Ms")
 
     document.getElementById('contador').innerHTML = `${diasRestantes}d ${horasRestantes}h ${minutosRestantes}m ${segundosRestantes}s`
 
